9315ff0d56efc8a3c2b0cc0bcfaf74e30152b35e
maikeljh <mjh191003@gmail.com>
2024-11-22 18:58:07 +0700

    feat: init fragments and navigation bar

--- app/src/main/java/com/example/culinairy/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/culinairy/ui/notifications/NotificationsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.culinairy.databinding.FragmentNotificationsBinding
+import com.example.culinairy.databinding.FragmentCaptureReceiptBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentCaptureReceiptBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class NotificationsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCaptureReceiptBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textCaptureReceipt
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
