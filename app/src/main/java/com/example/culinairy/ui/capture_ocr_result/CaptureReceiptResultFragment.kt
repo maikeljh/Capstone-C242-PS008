e73c1c92f6c0f101a3f3cbea3c73a770c1020c26
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-12-07 19:58:00 +0700

    feat: add item product list

---
+++ app/src/main/java/com/example/culinairy/ui/capture_ocr_result/CaptureReceiptResultFragment.kt
@@ -0,0 +1,36 @@
+package com.example.culinairy.ui.capture_ocr_result
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.culinairy.databinding.FragmentCaptureReceiptResultBinding
+
+class CaptureReceiptResultFragment : Fragment() {
+
+    private var _binding: FragmentCaptureReceiptResultBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val captureReceiptResultViewModel = ViewModelProvider(this)[CaptureReceiptResultViewModel::class.java]
+
+        _binding = FragmentCaptureReceiptResultBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
