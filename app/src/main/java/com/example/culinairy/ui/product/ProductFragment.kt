238ada2721c8304d6c98c19265a7bde75dcbcc8f
Kenneth Dave <dave.bahana@gmail.com>
2024-12-08 13:09:36 +0700

    feat: List product fragment

--- app/src/main/java/com/example/culinairy/ui/product/ProductFragment.kt
+++ app/src/main/java/com/example/culinairy/ui/product/ProductFragment.kt
@@ -1,29 +1,47 @@
-package com.example.culinairy.ui.capture_ocr
+package com.example.culinairy.ui.product
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.culinairy.databinding.FragmentCreateProductBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.culinairy.Adapter.ProductReceiptAdapter
+import com.example.culinairy.R
+import com.example.culinairy.model.Product
+import com.example.culinairy.databinding.FragmentListProductBinding
 
 class ProductFragment : Fragment() {
 
-    private var _binding: FragmentCreateProductBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentListProductBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var productAdapter: ProductReceiptAdapter
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentCreateProductBinding.inflate(inflater, container, false)
+    ): View? {
+        _binding = FragmentListProductBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // Initialize RecyclerView
+        recyclerView = binding.root.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Dummy data for the products
+        val productList = listOf(
+            Product("Item 1", 1, 10000.0, 10000.0),
+            Product("Item 2", 2, 20000.0, 40000.0),
+            Product("Item 3", 3, 15000.0, 45000.0)
+        )
+
+        // Initialize and set the adapter
+        productAdapter = ProductReceiptAdapter(productList)
+        recyclerView.adapter = productAdapter
+
         return root
     }
 
@@ -32,3 +50,4 @@ class ProductFragment : Fragment() {
         _binding = null
     }
 }
+
