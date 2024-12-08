a77e3f51822bdc9908e76da19e36f27da9eeffc5
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-12-08 20:56:16 +0700

    feat: fix fragments navigation

--- app/src/main/java/com/example/culinairy/MainActivity.kt
+++ app/src/main/java/com/example/culinairy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var fab: FloatingActionButton
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -38,9 +40,10 @@ class MainActivity : AppCompatActivity() {
 
         fab = binding.fab
         fab.setOnClickListener {
-            navController.navigate(R.id.navigation_capture_receipt)
-            val menuItem = navView.menu.findItem(R.id.navigation_capture_receipt)
-            menuItem.isChecked = true
+            if (navController.currentDestination?.id != R.id.navigation_capture_receipt) {
+                navController.navigate(R.id.navigation_capture_receipt)
+                updateBottomNavSelection(R.id.navigation_capture_receipt)
+            }
         }
 
         navView.setOnItemSelectedListener { item: MenuItem ->
@@ -62,7 +65,32 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 else -> false
+            }.also { isHandled ->
+                if (isHandled) updateBottomNavSelection(item.itemId)
             }
+        }
+    }
+
+    private fun updateBottomNavSelection(itemId: Int) {
+        val navView: BottomNavigationView = binding.navView
+        navView.menu.findItem(itemId).isChecked = true
+    }
+
+    override fun onBackPressed() {
+        if (!navController.navigateUp()) {
+            super.onBackPressed()
+        } else {
+            updateBottomNavSelection(navController.currentDestination?.id ?: R.id.navigation_home)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
