6d9941ce4ca2e9d9dec1f4609562fc49b8d1e4f1
Raylouis <sebastianraynard@gmail.com>
2024-12-10 21:57:53 +0700

    feat: update profile

--- app/src/main/java/com/example/culinairy/ui/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/culinairy/ui/profile/EditProfileViewModel.kt
@@ -3,20 +3,61 @@ package com.example.culinairy.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.culinairy.model.UpdateUserRequestBody
+import com.example.culinairy.repository.AuthRepository
+import kotlinx.coroutines.launch
 
 class EditProfileViewModel : ViewModel() {
-    private val _name = MutableLiveData<String>("Jhonson King")
+
+    private val repository = AuthRepository()
+
+    private val _name = MutableLiveData<String>()
     val name: LiveData<String> get() = _name
 
-    private val _email = MutableLiveData<String>("jhonking@gmail.com")
-    val email: LiveData<String> get() = _email
-
-    private val _address = MutableLiveData<String>("Jhonson King")
+    private val _address = MutableLiveData<String>()
     val address: LiveData<String> get() = _address
 
-    fun updateProfile(newName: String, newEmail: String, newAddress: String) {
-        _name.value = newName
-        _email.value = newEmail
-        _address.value = newAddress
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    fun fetchUser(token: String) {
+        viewModelScope.launch {
+            try {
+                val response = repository.getUser(token)
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    user?.let {
+                        _name.value = it.user.name
+                        _address.value = it.user.address
+                    }
+                } else {
+                    _error.value = "Failed to fetch user: ${response.message()}"
+                }
+            } catch (e: Exception) {
+                _error.value = "Error: ${e.message}"
+            } finally {
+            }
+        }
+    }
+
+    fun updateProfile(token: String, updateUserBody: UpdateUserRequestBody) {
+        viewModelScope.launch {
+            try {
+                val response = repository.updateUser(token, updateUserBody)
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    user?.let {
+                        _name.value = it.user.name
+                        _address.value = it.user.address
+                    }
+                } else {
+                    _error.value = "Failed to update user: ${response.message()}"
+                }
+            } catch (e: Exception) {
+                _error.value = "Error: ${e.message}"
+            } finally {
+            }
+        }
     }
 }
